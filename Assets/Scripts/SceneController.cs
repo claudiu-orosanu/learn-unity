381b6bf43216e2f2a0c5daf561f78663b2cf7fca
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-05-03 00:45:56 +0300

    Use dictionary, set shape color interactively

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     public List<Shape> gameShapes;
+    public Dictionary<string, Shape> shapesDict;
 
     // Start is called before the first frame update
     void Start()
+    {
+        shapesDict = new Dictionary<string, Shape>();
+        foreach (var shape in gameShapes)
+        {
+            shapesDict.Add(shape.Name, shape);
+        }
+    }
+
+    private void SetRedByName(string shapeName)
+    {
+        shapesDict[shapeName].SetColor(Color.red);
+    }
+
+    private void FindExample()
     {
         Shape octagon = gameShapes.Find(s => s.Name == "Octagon");
         octagon.SetColor(Color.red);
@@ -16,6 +31,14 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetRedByName("Square");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SetRedByName("Circle");
+        }
     }
 }
