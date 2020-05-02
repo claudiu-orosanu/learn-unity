eb20298c3cd6f072da845a9aee7b2ca928fe687d
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-05-03 00:53:26 +0300

    Use queue to set color of shapes in specific order

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,20 +6,42 @@ public class SceneController : MonoBehaviour
 {
     public List<Shape> gameShapes;
     public Dictionary<string, Shape> shapesDict;
+    public Queue<Shape> shapeQueue;
 
     // Start is called before the first frame update
     void Start()
     {
+        shapeQueue = new Queue<Shape>();
         shapesDict = new Dictionary<string, Shape>();
         foreach (var shape in gameShapes)
         {
             shapesDict.Add(shape.Name, shape);
         }
+
+        shapeQueue.Enqueue(shapesDict["Triangle"]);
+        shapeQueue.Enqueue(shapesDict["Square"]);
+        shapeQueue.Enqueue(shapesDict["Octagon"]);
+        shapeQueue.Enqueue(shapesDict["Circle"]);
     }
 
     private void SetRedByName(string shapeName)
     {
         shapesDict[shapeName].SetColor(Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (shapeQueue.Count > 0)
+            {
+                Shape shape = shapeQueue.Dequeue();
+                shape.SetColor(Color.blue);
+            } else {
+                Debug.Log("The shape queue is empty");
+            }
+        }
     }
 
     private void FindExample()
@@ -28,8 +50,7 @@ public class SceneController : MonoBehaviour
         octagon.SetColor(Color.red);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DictionaryExample()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
@@ -41,4 +62,5 @@ public class SceneController : MonoBehaviour
             SetRedByName("Circle");
         }
     }
+
 }
