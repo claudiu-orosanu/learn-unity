0f5403c096a8594552326243414a6a6ff4c2885c
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-05-03 17:32:25 +0300

    Add fields for score text and pause button in HUD controller, drag&drop in Unity inspector

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
+    public Text scoreText;
+    public Text pauseButtonText;
+
     // Start is called before the first frame update
     void Start()
     {
