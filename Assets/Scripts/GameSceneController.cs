a5f27b690eed84ce0d95a1930124a2c2bfef0467
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-05-03 11:18:31 +0300

    Add new scene, add player, change horizontal position of player using arrow keys

---
+++ Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+    public float playerSpeed;
+    public Vector3 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = 10;
+        screenBounds = GetScreenBounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Vector3 GetScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 screenVector = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
+        return screenVector;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Shape
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetColor(Color.yellow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // GetAxis is mapped to the arrow keys, Horizontal means left and right arrow keys (left will have negative value, right positive)
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        // Debug.Log($"horizontalMovement = {horizontalMovement}");
+
+        if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon) // comparing to Epsilon to account for floating point precision errors
+        {
+            // Debug.Log($"deltaTime = {Time.deltaTime}. transformPositionX = {transform.position.x}");
+            horizontalMovement = horizontalMovement * Time.deltaTime;
+            horizontalMovement += transform.position.x;
+
+            Debug.Log($"transform.position = {transform.position}");
+            transform.position = new Vector2(horizontalMovement, transform.position.y);
+            // Debug.Log($"horizontalMovement = {horizontalMovement}. transform.position = {transform.position}");
+
+        }
+    }
+}
