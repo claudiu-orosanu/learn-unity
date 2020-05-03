adb817fc5dbe0e6bf726cf038e22a1079b0dd725
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-05-03 10:14:35 +0300

    Example of running two things in parallel using coroutines

--- Assets/Scripts/CoroutineSceneController.cs
+++ Assets/Scripts/CoroutineSceneController.cs
@@ -17,14 +17,28 @@ public class CoroutineSceneController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // SetShapesRed();
+            StartCoroutine(CountToNumber(25000));
             StartCoroutine(SetShapesBlue());
-            Time.timeScale = 0; // pauses the game
+            // Time.timeScale = 0; // pauses the game
+            Debug.Log("Keypress complete.");
+        }
+    }
+
+    private IEnumerator CountToNumber(int numberToCountTo)
+    {
+        // running this as a normal method will hang the program when pressing Space
+        // running as a coroutine will run it in parallel with the rest of the game
+        for (int i = 0; i < numberToCountTo; i++)
+        {
+            Debug.Log(i);
+            yield return null;
         }
     }
 
     private IEnumerator SetShapesBlue()
     {
+        Debug.Log("Start changing colors");
+
         foreach (var shape in gameShapes)
         {
             shape.SetColor(Color.blue);
