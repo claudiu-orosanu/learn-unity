374ae4a51fd1b1fc3d3ce8ff5ff9d52b9c3bca07
Claudiu Orosanu <orosanu.claudiu@gmail.com>
2020-08-11 21:31:52 +0300

    Rotate rocket. Frame-rate independence with "Time.deltaTime".

--- CompleteUnity3d/3_Project_Boost/Assets/Rocket.cs
+++ CompleteUnity3d/3_Project_Boost/Assets/Rocket.cs
@@ -6,7 +6,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public float acceleration = 1000f;
+
     private Rigidbody _rigidbody;
+
+    //     void Awake () {
+//        // set frame rate to check Time.deltaTime works
+// #if UNITY_EDITOR
+//         QualitySettings.vSyncCount = 0;  // VSync must be disabled
+//         Application.targetFrameRate = 60;
+// #endif
+//     }
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +35,20 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
             // print("Thrusting!");
-            _rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up * (Time.deltaTime * acceleration));
+            // _rigidbody.AddRelativeTorque(Vector3.up * (Time.deltaTime * acceleration));
+
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            // print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Rotating right");
+            transform.Rotate(Vector3.back);
         }
     }
 }
